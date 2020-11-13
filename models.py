14b5c361e8eda355c7264772702644aea2cf957a
winu <haruto@hacarus.com>
2020-11-14 08:56:35 +0900

    bug fix

--- models.py
+++ models.py
@@ -6,8 +6,7 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     """ User model """
-
-    __tablename__ = "users"
+    __tablename__ = "USERS"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     hashed_pswd = db.Column(db.String(), nullable=False)
