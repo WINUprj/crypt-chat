ee31b63cee2eac48cafd5d9bbc97e87a30e3ba8b
winu <haruto@hacarus.com>
2020-11-14 14:44:15 +0900

    bug fix

--- application.py
+++ application.py
@@ -17,7 +17,6 @@ app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-db.create_all()
 
 # Initialize login manager
 login = LoginManager(app)
