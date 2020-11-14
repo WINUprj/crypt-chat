9580450c02ba9cd33868ad5bc390d52a4ee894be
winu <haruto@hacarus.com>
2020-11-14 12:27:38 +0900

    bug fix

--- create.py
+++ create.py
@@ -9,7 +9,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db.init_app(app)
 
 def main():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 if __name__ == "__main__":
     with app.app_context():
